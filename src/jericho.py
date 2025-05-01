214fac282a2fae605c527de91382ef7b34c9faa8
VioletRimberg <124800011+VioletRimberg@users.noreply.github.com>
2025-05-01 13:10:27 +0200

    Fixed Maintenance Command
    Used the wrong settings thing, silly me.

--- src/jericho.py
+++ src/jericho.py
@@ -684,19 +684,18 @@ async def text_maintenance(interaction: discord.Interaction):
     if any(role.id == SETTINGS.MAINTENANCE_ROLE_ID for role in interaction.user.roles):
         try:
             global MESSAGE_PROVIDER
-            MESSAGE_PROVIDER = MessageProvider.from_gsheets(
-                SETTINGS.MESSAGE_PROVIDER_URL
-            )
-            info(f"User {interaction.user.name} attempted to refresh google sheet data")
+            MESSAGE_PROVIDER = MessageProvider.from_gsheets(SETTINGS.GOOGLE_SHEET_MESSAGEPROVIDER_ID)
             await interaction.response.send_message(
                 MESSAGE_PROVIDER("MAINTENANCE_INI"), ephemeral=True
             )
             await interaction.followup.send(
                 MESSAGE_PROVIDER("MAINTENANCE_SUCCESS"), ephemeral=True
             )
+
         except Exception as e:
             info(f"Refresh failed with error: {e}")
-            await interaction.followup.send(
+            # ⚠️ direct response, not followup
+            await interaction.response.send_message(
                 MESSAGE_PROVIDER("MAINTENANCE_ERROR", error=e), ephemeral=True
             )
     else:
